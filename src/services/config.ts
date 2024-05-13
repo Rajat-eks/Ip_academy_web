0553b56f3499a259ef3ef54c66ba59e8c58a1003
Rajat <rajat.verma@effectualservices.in>
2024-05-13 09:45:03 +0530

    code

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,17 +1,24 @@
 import * as axios from "axios";
 
-const ACCESS_TOKEN = localStorage.getItem("access_token")
-  ? `Bearer ${localStorage.getItem("access_token")}`
-  : undefined;
+const ACCESS_TOKEN = () => {
+  return localStorage.getItem("access_token")
+    ? `Bearer ${localStorage.getItem("access_token")}`
+    : undefined;
+};
 
 let axiosObject = axios.default.create();
-axiosObject.defaults.baseURL = "https://ip-academy-apis.onrender.com/api/v1/";
+// axiosObject.defaults.baseURL = "https://ip-academy-apis.onrender.com/api/v1/";
+axiosObject.defaults.baseURL = "http://localhost:8080/api/v1/";
 axiosObject.defaults.timeout = 8000;
 axiosObject.defaults.headers.common = {
   "Content-Type": "application/json",
   Accept: "application/json",
-  Authorization: ACCESS_TOKEN,
+ 
 };
+axiosObject.interceptors.request.use(config => {
+  config.headers.Authorization = ACCESS_TOKEN();
+  return config;
+});
 
 axiosObject.interceptors.request.use(
   async function (config: any) {
